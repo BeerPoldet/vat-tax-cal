7907bfa7f24443fd0fce791a97f38bca8745d80c
keng <keng@pupasoft.com>
2019-03-25 14:49:14 +0700

    =Add item price function

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const inputs = require('items.json');
+const inputs = require('./items.json')
+
+const itemPrice = item => {
+  return item
+    ? item.reduce((acc, item) => acc + item.price * item.amount, 0)
+    : 0
+}
+
+
+
